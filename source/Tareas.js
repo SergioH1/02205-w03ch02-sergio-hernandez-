72f8ee496798c59de36bf49d5e21cc9aaf5a3617
Sergio H <sergiohernandezroman@gmail.com>
2022-05-24 18:06:55 +0200

    Add css an index

---
+++ source/Tareas.js
@@ -0,0 +1,12 @@
+export class Tareas {
+    tarea;
+    responsable;
+    completeda;
+    id;
+    constructor(tarea, responsable, completeda, id) {
+        this.tarea = tarea;
+        this.responsable = responsable;
+        this.completeda = completeda;
+        this.id = id;
+    }
+}
---
+++ source/Tareas.ts
@@ -0,0 +1,8 @@
+export class Tareas {
+    constructor(
+        public tarea: string,
+        public responsable: string,
+        public completeda: false,
+        public id: string
+    ) {}
+}
